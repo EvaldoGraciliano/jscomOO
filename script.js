18b64f6941d59bbdb4cfa134888797b0049af58b
Evaldo Graciliano <neto.graciliano@hotmail.com>
2021-07-23 20:59:30 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,21 @@
+class Produto {
+
+    constructor(){
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor = 0;
+
+    }
+
+    adicionar(){
+        alert('Vamos adicionar um produto')
+    }
+
+    excluir(){
+        alert('Produto foi deletado')
+    }
+
+}
+
+
+var produto = new Produto();
